2b7b0056f7c36ef75982f2dd55618ab02fe2a743
sHale <sally.hale@homeport.org.uk>
2018-05-09 16:14:56 +0100

    Change routes to heroku

--- src/api.js
+++ src/api.js
@@ -1,43 +1,44 @@
 export const fetchArticles = topic => {
   if (topic) {
-    return fetch(`http://localhost:9090/api/topics/${topic}/articles`).then(
+    return fetch(`https://still-meadow-57011.herokuapp.com/api/topics/${topic}/articles`).then(
+      
       res => res.json()
     );
   } else {
-    return fetch(`http://localhost:9090/api/articles`).then(res => res.json());
+    return fetch(`https://still-meadow-57011.herokuapp.com/api/articles`).then(res => res.json());
   }
 };
 
 export const fetchArticleById = articleID => {
-  return fetch(`http://localhost:9090/api/articles/${articleID}`).then(res => 
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/articles/${articleID}`).then(res => 
     res.json()
   )
 }
 
 export const fetchArticlesByUsername = username => {
-  return fetch(`http://localhost:9090/api/articles/${username}`).then(res => 
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/articles/${username}`).then(res => 
     res.json()
   )
 }
 
 export const fetchUserInfo = userInfo => {
-  return fetch(`http://localhost:9090/api/users/${userInfo}`).then(res => 
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/users/${userInfo}`).then(res => 
     res.json()
   );
 };
 
 export const fetchTopics = () => {
-  return fetch("http://localhost:9090/api/topics").then(res => res.json());
+  return fetch("https://still-meadow-57011.herokuapp.com/api/topics").then(res => res.json());
 };
 
 export const fetchComments = articleID => {
-  return fetch(`http://localhost:9090/api/articles/${articleID}/comments`).then(
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/articles/${articleID}/comments`).then(
     res => res.json()
   )
 };
 
 export const postComment = (articleID, commentBody) => {
-  return fetch(`http://localhost:9090/api/articles/${articleID}/comments`, {
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/articles/${articleID}/comments`, {
     headers: {
       Accept: "application/json",
       "Content-Type": "application/json"
@@ -48,19 +49,19 @@ export const postComment = (articleID, commentBody) => {
 };
 
 export const addVoteArticle = (articleID, articleVote) => {
-  return fetch(`http://localhost:9090/api/articles/${articleID}?vote=up&articleVote=${articleVote}`, {
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/articles/${articleID}?vote=up&articleVote=${articleVote}`, {
     method: "PUT"
   }).then(res => res.json());
 };
 
 export const downVoteArticle = (articleID, articleVote) => {
-  return fetch(`http://localhost:9090/api/articles/${articleID}?vote=down&articleVote=${articleVote}`, {
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/articles/${articleID}?vote=down&articleVote=${articleVote}`, {
     method: "PUT"
   }).then(res => res.json());
 };
 
 export const deleteComment = commentID => {
-  return fetch(`http://localhost:9090/api/comments/${commentID}`, {
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/comments/${commentID}`, {
     method: "DELETE"
   }).then(res => {
     res.json();
@@ -68,13 +69,13 @@ export const deleteComment = commentID => {
 };
 
 export const addVoteComment = commentID => {
-  return fetch(`http://localhost:9090/api/comments/${commentID}?vote=up`, {
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/comments/${commentID}?vote=up`, {
     method: "PUT"
   }).then(res => res.json());
 };
 
 export const downVoteComment = commentID => {
-  return fetch(`http://localhost:9090/api/comments/${commentID}?vote=down`, {
+  return fetch(`https://still-meadow-57011.herokuapp.com/api/comments/${commentID}?vote=down`, {
     method: "PUT"
   }).then(res => res.json());
 };
